89c45320e394fc13f745aa154d076e12e4e9d8e4
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-19 16:23:01 +0530

    Update App.jsx

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,7 +2,10 @@ import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 function App() {
     return(
-      <Header></Header>
+      <>
+        <Header/>
+        <Footer/>
+      </>
     );
 }
 
