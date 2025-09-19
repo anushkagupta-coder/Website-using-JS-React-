71a435faf0ca1e13ef21a9ab46ec0c1d0ea11141
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-20 00:13:48 +0530

    Update myComponent.jsx

--- my-react-app/src/myComponent.jsx
+++ my-react-app/src/myComponent.jsx
@@ -3,6 +3,7 @@ import React, {useState}from 'react';
 function MyComponent(){
 
     const [name, setName] = useState();
+    const[age,setAge]=useState(0);
 
     const updateName=() => {
         setName("hermoine")
@@ -11,6 +12,9 @@ function MyComponent(){
     return(<div>
         <p>Name: {name}</p>
         <button onClick={updateName}>Set Name</button>
+
+        <p>Age:{age}</p>
+        <button onClick={incrementAge}>Increment Age</button>
     </div>)
 
 }
