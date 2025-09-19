b8bf3aa99469829683e16eb4c30f16059b088a66
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-19 23:48:07 +0530

    Update myComponent.jsx

--- my-react-app/src/myComponent.jsx
+++ my-react-app/src/myComponent.jsx
@@ -1,7 +1,15 @@
 import React, {useState}from 'react';
 
 function myComponent(){
-    const [name, setName] = useState();
+    let [name, setName] = useState();
+    const updateName=() => {
+        name="hermoine";
+        console.log(name);
+    }
+    return(<div>
+        <p>Name: {name}</p>
+        <button onClick={updateName}>Set Name</button>
+    </div>)
 
 }
 export default myComponent
