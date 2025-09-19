d4631818899d85acdcd2b74fc41d5f6428deac81
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-19 16:57:21 +0530

    Update Card.jsx

--- my-react-app/src/Card.jsx
+++ my-react-app/src/Card.jsx
@@ -4,7 +4,7 @@ function Card(){
             <div className="card">
                 <img className="card-image" src="https://images.contentstack.io/v3/assets/bltcedd8dbd5891265b/blt4a4af7e6facea579/6668df6ceca9a600983250ac/beautiful-flowers-hero.jpg?q=70&width=828&auto=webp" alt="profile picture"></img>
                 <h2 className="card-title">bro code</h2>
-                <p>I am 3rd yr engg student at Pune</p>
+                <p className="card-text">I am 3rd yr engg student at Pune</p>
             </div>
         );
 }
