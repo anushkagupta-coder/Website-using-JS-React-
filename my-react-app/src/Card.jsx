c58337efddee77fe8eb146d429f85e638bdcb113
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-19 16:43:39 +0530

    css

--- my-react-app/src/Card.jsx
+++ my-react-app/src/Card.jsx
@@ -2,7 +2,7 @@ function Card(){
 
         return(
             <div className="card">
-                <img src ="https://via.placeholder.com/150 " alt="profile picture"></img>
+                <img src="https://via.placeholder.com/150 " alt="profile picture"></img>
                 <h2>bro code</h2>
                 <p>I am 3rd yr engg student at Pune</p>
             </div>
