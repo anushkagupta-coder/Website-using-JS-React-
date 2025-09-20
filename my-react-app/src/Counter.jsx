14f8ab18ad732172142f185357166a49802c220d
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-20 11:37:19 +0530

    Update Counter.jsx

--- my-react-app/src/Counter.jsx
+++ my-react-app/src/Counter.jsx
@@ -2,5 +2,15 @@ import React,{useState} from 'react';
 function Counter(){
     const[count,setCount]=useState(0);
 
+    const increment=()=> {
+        setCount(count+1);
+    }
+    const decrement=()=> {
+        setCount(count-1);
+    }
+    const reset=()=> {
+        setCount(0);
+    }
+
 }
 export default Counter;
