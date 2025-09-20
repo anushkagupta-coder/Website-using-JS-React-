534724266e958d29ff55c2d16b14dc183e44b2f4
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-20 12:10:25 +0530

    Update myComponent.jsx

--- my-react-app/src/myComponent.jsx
+++ my-react-app/src/myComponent.jsx
@@ -3,14 +3,21 @@ import React, {useState}from 'react';
 
 function MyComponent(){
     const [name,setName]=useState("");
+    const[quantity,setQuantity]=useState(); 
 
     function handleNameChange(event){
         setName(event.target.value);
     }
 
+    function handleQuantityChange(event){
+        setQuantity(event.target.value);
+    }
+
     return(<div>
         <input value={name} onChange={handleNameChange}/>
         <p>ENTER UR NAME: {name}</p>
+        <input value={quantity} onChange={handleQuantityChange} type="number"/>
+        <p>Quantity:</p>
     </div>)
 }
 /*function MyComponent(){
