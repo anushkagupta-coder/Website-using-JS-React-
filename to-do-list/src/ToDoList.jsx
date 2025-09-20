00c75aa7fa680e878f48fc8e8ccf39c173a9022f
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-20 19:51:31 +0530

    to-do-app

--- to-do-list/src/ToDoList.jsx
+++ to-do-list/src/ToDoList.jsx
@@ -2,21 +2,34 @@ import React ,{useState}from 'react'
 function ToDoList(){
 
     const [tasks,setTasks]=useState(["eat breakffast","take shsower"]);
-    const[newTask,setNewTask]=newState=("");
+    const[newTask,setNewTask]=useState("");
 
     function handleInputChange(event){
         setNewTask(event.target.value)
     }
 
     function addTask(){
-
+        if(newTask.trim() !==""){
+            setTasks(t=> [...t,newTask]);
+            setNewTask("");
+        }
     }
 
     function deleteTask(index){
 
+        const updatedTasks=tasks.filter((_,i)=> i !==index);
+        setTasks(updatedTasks);
+
     }
 
     function moveTaskUp(index){
+        if(index > 0 ){
+            const updatedTasks = [...tasks];
+            [updatedTasks[index]], [updatedTasks[index-1]] = 
+            [updatedTasks[index-1]],[updatedTasks[index]];
+
+            setTasks(updatedTasks);
+        }
 
     }
 
