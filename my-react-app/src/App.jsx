5ce9823b2631b2c70421cdfc54aedee9681918be
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-22 14:56:58 +0530

    component3

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -49,6 +49,7 @@ import List from './List.jsx'
 import MyComponent from './MyComponent.jsx'
 import Counter from './Counter.jsx'
 import MyComponent2 from './MyComponent2.jsx'
+import MyComponent3 from "./MyComponent3";
 import { useCallback, useContext, useEffect, useReducer } from 'react'
 
 function App() {
@@ -74,6 +75,7 @@ function App() {
         <Counter/>
         <MyComponent/>
         <MyComponent2/>
+        <MyComponent3/>
         <List items={fruits} category="Fruits"/>
         <List items={vegetables} category ="Vegetables"/>
         <UserGreeting isLoggedIn={true} username="anushka"/>
