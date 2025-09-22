4b6ed0472c96c2968ece24b513372c95f5210990
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-22 15:44:47 +0530

    Update digitalClock.jsx

--- digital-clock/src/digitalClock.jsx
+++ digital-clock/src/digitalClock.jsx
@@ -4,12 +4,19 @@ function digitalClock(){
 
     const [time,setTime] = useState(new Date());
     useEffect(()=>{
+        const intervalId=setInterval(()=>{
+            setTime(new Date());
+        },1000);
+
+        return()=>{
+            clearInterval(intervalId);
+        }
 
     },[]);
 
     return(<>
         <div className ="clock-container">
-            <div classNAme="clock">
+            <div className="clock">
                 <span>00:00:00</span>
             </div>
         </div>
