8909e5136a55c56f4669bb88c11f31d5949aa18c
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-22 14:59:18 +0530

    Update myComponent.jsx

--- my-react-app/src/myComponent.jsx
+++ my-react-app/src/myComponent.jsx
@@ -59,35 +59,4 @@ function MyComponent(){
 
 
 
-/*function MyComponent(){
-
-    const [name, setName] = useState("ANUSHKA");
-    const[age,setAge]=useState(0);
-    const [isEmployed,setIsEmployed]= useState(false);
-
-    const updateName=() => {
-        setName("HERMOINE")
-    }
-
-    const incrementAge=()=>{
-        setAge(age+1);
-
-    }
-
-    const toggleEmployedStatus=()=>{
-        setIsEmployed(!isEmployed);
-    }
-
-    return(<div>
-        <p>Name: {name}</p>
-        <button onClick={updateName}>Set Name</button>
-
-        <p>Age:{age}</p>
-        <button onClick={incrementAge}>Increment Age</button>
-
-        <p>Is employed:{isEmployed ? "yes": "no"}</p>
-        <button onClick={toggleEmployedStatus}>toggle status</button>
-    </div>)
-
-}*/
 export default MyComponent
