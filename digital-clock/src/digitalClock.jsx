db00cb739af90e0d02c7d4aa962a06d365241246
anushkagupta-coder <anushka.kufam22@sinhgad.edu>
2025-09-22 15:37:41 +0530

    digital clock

---
+++ digital-clock/src/digitalClock.jsx
@@ -0,0 +1,18 @@
+import React,{useStae,useEffect} from `react`;
+
+function digitalClock(){
+
+
+
+    return(<>
+        <div className ="clock-container">
+            <div classNAme="clock">
+                <span>00:00:00</span>
+            </div>
+        </div>
+    
+    </>);
+}
+
+
+export default digitalClock
